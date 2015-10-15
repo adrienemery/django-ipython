5884506ced57f5a2505427567513772fc4b67eb6
Adrien Emery <adrien.emery@gmail.com>
2015-10-15 00:01:16 -0700

    update download url in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'django-ipython',
   packages = ['django_ipython'],
-  version = '0.1',
+  version = '0.1.1',
   description = 'An IPython extension for using django with ipython notbeook',
   author = 'Adrien Emery',
   author_email = 'adrien.emery@gmail.com',
   url = 'https://github.com/adrienemery/django-ipython', # use the URL to the github repo
-  download_url = 'https://github.com/adrienemery/django-ipython/releases/tag/0.1',
+  download_url = 'https://github.com/adrienemery/django-ipython/releases/tag/v0.1.1',
   keywords = ['ipython', 'notebook', 'jupyter', 'django'],
   classifiers = [],
 )
